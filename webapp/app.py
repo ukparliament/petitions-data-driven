90b83aec9caed77d1b72075b556092445f48a6e4
Rebecca Appleyard <rklappleyard@gmail.com>
2016-07-12 17:29:15 +0100

    Added basic index and show pages for petitions

--- webapp/app.py
+++ webapp/app.py
@@ -1,7 +1,12 @@
 import os
 import socket
+import httplib
+import json
 
 from flask import Flask
+from flask import render_template
+
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -11,7 +16,37 @@ def hello():
     return 'Hello '+provider+'!'
 print(socket.gethostname())
 
+@app.route('/petitions')
+def petitions():
+	conn = httplib.HTTPConnection("data-driven.ci.ukpds.org")
+	try:
+		conn.request("GET", "/petitions.json")
+		response = conn.getresponse()
+		repsonse_body = response.read()
+	finally:
+		conn.close()
+
+	data = json.loads(repsonse_body)
+	return render_template("petitions/index.html", data = data)
+
+@app.route('/petitions/<id>')
+def petition(id):
+	conn = httplib.HTTPConnection("data-driven.ci.ukpds.org")
+	try:
+		conn.request("GET", "/petitions/{0}.json".format(id))
+		response = conn.getresponse()
+		repsonse_body = response.read()
+	finally:
+		conn.close()
+
+	data = json.loads(repsonse_body)
+	return render_template("petitions/show.html", data = data)
+
+@app.route('/constituency/<id>')
+def constituency(id):
+	return "constituency"
+
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
